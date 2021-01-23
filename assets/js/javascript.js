3b6908bce734a41a6aa3e166ed76dce7743f0918
expertape <expertape@pop-os.localdomain>
2021-01-23 11:16:15 +0100

    New commands & Some improvements

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -49,8 +49,7 @@ var prompt = {
     clear: function(){
         prompt.commands.push("clear");
         prompt.body.html("");
-        k = `<div class="terminal__text">`+ prompt.welcome_msg + `</div>
-              <div class="terminal_main">
+        k = `<div class="terminal_main">
                 <div class="terminal_input_left">
                     <span class="user_msg">you@gcm:</span><span class="user_loc">~</span><span class="user_doll">$</span>
                 </div>
@@ -66,8 +65,11 @@ var prompt = {
         k.addEventListener('keydown', (e) => {
             if (e.keyCode == 13){
                 i = 0;
-                if(e.target.value == "clear"){
+                var command = e.target.value;
+                if(command == "clear"){
                     prompt.clear();
+                } else if(/^light (on|off)$/.test(command)) {
+                    prompt.light(e);
                 } else {
                     prompt.changeContent(e);
                 }
@@ -77,6 +79,17 @@ var prompt = {
             }
         });
     },
+    light: function(e) {
+      var command = e.target.value;
+      if (command.split(" ").splice(-1) == "off"){
+        $('#overlay').fadeIn(1000);
+        $('.terminal').addClass('highlight');
+      } else {
+        $('#overlay').fadeOut(1000);
+        $('.terminal').removeClass('highlight');  
+      }
+      prompt.changeContent(e);
+    },
     changeContent: function(e){
         e.target.setAttribute("disabled","disabled");
         var div = document.createElement("DIV");
@@ -85,50 +98,55 @@ var prompt = {
         prompt.commands.push(e.target.value);
         switch(e.target.value){
           case "help":
-            div.innerHTML = "HELP\r\n" + 
-                              "------------------------------------------------------------\r\n" +
-                              "COMMAND      DESCRIPTION\r\n\r\n" +
-                              "help         list all the commands you could run\r\n" +
-                              "clear        clear terminal screen\r\n" +
-                              "contact      display my name and contact details\r\n" +
-                              "summary      display a brief qualifications summary\r\n" +
-                              "education    display list of the highest levels of education\r\n" +
-                              "experience   display work experience\r\n" +
-                              "skills       display list of skills\r\n" +
-                              "hobbies      display list of hobbies\r\n";
+            div.innerHTML =   "<b>COMMAND</b>         <b>DESCRIPTION</b>\r\n\r\n" +
+                              "<i>help</i>             list all the commands you could run\r\n" +
+                              "<i>clear</i>            clear terminal screen\r\n" +
+                              "<i>contact</i>          display my name and contact details\r\n" +
+                              "<i>summary</i>          display a brief qualifications summary\r\n" +
+                              "<i>education</i>        display list of the highest levels of education\r\n" +
+                              "<i>experience</i>       display work experience\r\n" +
+                              "<i>skills</i>           display list of skills\r\n" +
+                              "<i>hobbies</i>          display list of hobbies\r\n" +
+                              "<i>light [on off]</i>   turn on/off the light";
             break;
           case "contact":
-            div.innerHTML = " > First and last name: <strong> Gonzalo Cuesta Marín </strong>\r\n" +
-                            " > City and Country: <strong> Burgos, Spain </strong>\r\n" +
-                            " > E-mail: <strong> <a href='mailto:goncuesma@gmail.com'>goncuesma@gmail.com</a></strong>\r\n" + 
-                            " > LinkedIn: <strong> <a href='https://www.linkedin.com/in/gcm97' target='_blank'>//gcm97</a> </strong>\r\n" +
-                            " > GitHub: <strong> <a href='https://github.com/GonCuesMa/' target='_blank'>/GonCuesMa</a> </strong>";
+            div.innerHTML = " > First and last name: <b> Gonzalo Cuesta Marín </b>\r\n" +
+                            " > City and Country: <b> Burgos, Spain </b>\r\n" +
+                            " > E-mail: <b> <a href='mailto:goncuesma@gmail.com'>goncuesma@gmail.com</a></b>\r\n" + 
+                            " > LinkedIn: <b> <a href='https://www.linkedin.com/in/gcm97' target='_blank'>//gcm97</a> </b>\r\n" +
+                            " > GitHub: <b> <a href='https://github.com/GonCuesMa/' target='_blank'>/GonCuesMa</a> </b>";
             break;
           case "summary":
             div.innerHTML = "\r\n > As a recent graduate in Computer Science, I've had the opportunity to carry out work practices where I've could participate in an international project, working as a team and applying values like responsibility, communication, learning, flexibility and organization. My main areas of interest are web development/design and artificial intelligence.\r\n";
             break;
           case "education":
-            div.innerHTML = " > <strong><a href='https://www.ubu.es/' target='_blank'>University of Burgos</a></strong>\r\n" +
+            div.innerHTML = " > <b><a href='https://www.ubu.es/' target='_blank'>University of Burgos</a></b>\r\n" +
                             "   * <a href='https://www.ubu.es/grado-en-ingenieria-informatica' target='_blank'>Computer Engineering</a> | September 2016 - June 2020\r\n";
             break;
           case "experience":
-            div.innerHTML = " > <strong><a href='https://www.cenieh.es/' target='_blank'>CENIEH</a></strong>\r\n" +
+            div.innerHTML = " > <b><a href='https://www.cenieh.es/' target='_blank'>CENIEH</a></b>\r\n" +
                             "   * Collaborator of <a href='https://ariadne-infrastructure.eu/' target='_blank'>ARIADNEplus</a> project | February 2020 - July 2020\r\n";
             break;
           case "skills":
-            div.innerHTML = " > Programming languages: <strong> Java, PHP, JavaScript (jQuery), Python </strong>\r\n" +
-                            " > Databases: <strong> PostgreSQL, MySQL </strong>\r\n" +
-                            " > Frameworks: <strong> Zend Framework </strong>\r\n" + 
-                            " > Technologies: <strong> Docker, Kubernetes,  MVC</strong>\r\n" +
-                            " > Other: <strong> Cloud Services (Google Cloud) </strong>";
+            div.innerHTML = " > Programming languages: <b> Java, PHP, JavaScript (jQuery), Python </b>\r\n" +
+                            " > Databases: <b> PostgreSQL, MySQL </b>\r\n" +
+                            " > Frameworks: <b> Zend Framework </b>\r\n" + 
+                            " > Technologies: <b> Docker, Kubernetes,  MVC</b>\r\n" +
+                            " > Other: <b> Cloud Services (Google Cloud) </b>";
             break;
           case "hobbies":
-            div.innerHTML = " > <strong> Motorcycling </strong> \r\n" +
-                            " > <strong> Cooking </strong>\r\n" +
-                            " > <strong> Travelling </strong>\r\n" + 
-                            " > <strong> Taking free online classes </strong>\r\n" +
-                            " > <strong> Electronics repair </strong>";
+            div.innerHTML = " > <b> Motorcycling </b> \r\n" +
+                            " > <b> Cooking </b>\r\n" +
+                            " > <b> Travelling </b>\r\n" + 
+                            " > <b> Taking free online classes </b>\r\n" +
+                            " > <b> Electronics repair </b>";
             break;
+          case "light on":
+            div.innerHTML = " > <b> The lights went on! Type 'lights off' to turn them off.  </b> \r\n";
+            break;
+          case "light off":
+            div.innerHTML = " > <b> The lights went out! Type 'lights on' to turn them on. </b> \r\n";
+            break;  
           default:
             div.textContent = e.target.value + ": command not found";
         }
